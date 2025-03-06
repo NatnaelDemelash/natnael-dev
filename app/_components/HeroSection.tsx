bfbdc5d0e9a2a7ae44eac7121b7d57f28939828f
NatnaelDemelash <natidemelash074@gmail.com>
2025-03-06 22:37:44 +0300

    basic components and folder structure

---
+++ app/_components/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <section id="home" className="min-h-screen flex items-center">
+      <div>
+        <h1>Hero Section</h1>
+        {/* Add your hero content here */}
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import {
+  BriefcaseBusiness,
+  Contact as ContactIco,
+  House,
+  UserRound,
+} from 'lucide-react';
+
+import HeroSection from './_components/HeroSection';
+import Skills from './_components/Skills';
+import ProjectsSection from './_components/ProjectsSection';
+import Footer from './_components/Footer';
+import { Navbar } from './_components/ui/Navbar';
+
+const navItems = [
+  { name: 'Home', link: '#home', icon: <House /> },
+  { name: 'Work', link: '#work', icon: <BriefcaseBusiness /> },
+  { name: 'About', link: '#about', icon: <UserRound /> },
+  { name: 'Contact', link: '#contact', icon: <ContactIco /> },
+];
+
+const Homepage = () => {
+  return (
+    <main className="flex flex-col px-5 sm:px-10 relative">
+      <div className="max-w-7xl mx-auto w-full">
+        <Navbar navItems={navItems} />
+        <HeroSection />
+        <Skills />
+        <ProjectsSection />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Homepage;
